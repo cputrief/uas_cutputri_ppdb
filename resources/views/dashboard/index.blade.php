57e9b6b15fbf34f44b71cfe7cf1051a1d82d012e
cputrief <cutputriefrina@gmail.com>
2025-07-14 15:09:41 +0700

    test

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -15,30 +15,34 @@
 <div class="content">
     <div class="row">
         <div class="col-lg-3 col-sm-6">
-            <div class="card">
-                <div class="content">
-                    <div class="row">
-                        <div class="col-xs-5">
-                            <div class="icon-big icon-warning text-center">
-                                <i class="ti-blackboard"></i>
+            <div class="card" style="cursor: pointer;">
+                <a href="/kelas" style="text-decoration: none; color: inherit;">
+                    <div class="content">
+                        <div class="row">
+                            <div class="col-xs-5">
+                                <div class="icon-big icon-warning text-center">
+                                    <i class="ti-blackboard"></i>
+                                </div>
+                            </div>
+                            <div class="col-xs-7">
+                                <div class="numbers">
+                                    <p>Kelas</p>
+                                    30
+                                </div>
                             </div>
                         </div>
-                        <div class="col-xs-7">
-                            <div class="numbers">
-                                <p>Kelas</p>
-                                30 
+                        <div class="footer">
+                            <hr />
+                            <div class="stats">
+                                <i class="ti-reload"></i> Updated now
                             </div>
                         </div>
                     </div>
-                    <div class="footer">
-                        <hr />
-                        <div class="stats">
-                            <i class="ti-reload"></i> Updated now
-                        </div>
-                    </div>
-                </div>
+                </a>
             </div>
         </div>
+        
+        
         <div class="col-lg-3 col-sm-6">
             <div class="card">
                 <div class="content">
